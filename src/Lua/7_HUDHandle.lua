3752e529d63fbac9d60e3fc6676897fd467ed41a
Blur <danielthesmartypants2017@gmail.com>
2023-10-28 21:04:42 -0500

    snap to top

--- src/Lua/7_HUDHandle.lua
+++ src/Lua/7_HUDHandle.lua
@@ -342,7 +342,7 @@ local gamemode_hud = function(v,player)
 	if not PTSR.pizzatime then return end
 	
 
-	v.drawString(320, 0, "\x8A"..currentGamemode, V_SNAPTORIGHT|V_50TRANS|V_ADD, "thin-right")
+	v.drawString(320, 0, "\x8A"..currentGamemode, V_SNAPTORIGHT|V_SNAPTOTOP|V_50TRANS|V_ADD, "thin-right")
 end
 
 customhud.SetupItem("PTSR_bar", hudmodname, bar_hud, "game", 0)

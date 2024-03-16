2458eca6fa8ab7c5010efc21c7d0e25bfc8cf26d
Blur <danielthesmartypants2017@gmail.com>
2024-03-15 22:09:06 -0500

    fix death chat not showing in all gamemodes

--- src/Lua/Hooks/PlayerDeath.lua
+++ src/Lua/Hooks/PlayerDeath.lua
@@ -10,11 +10,9 @@ addHook("MobjDeath", function(mobj)
 	if PTSR.pizzatime then
 		if not player.pizzaface then
 			if CV_PTSR.showdeaths.value then
-				if PTSR.gamemode == 1 or PTSR.gamemode == 2 then
-					chatprint("\x82*"..player.name.."\x82 has died.")
-					if DiscordBot then
-						DiscordBot.Data.msgsrb2 = $ .. "[" .. #player .. "]:skull: **" .. player.name .. "** died.\n"
-					end
+				chatprint("\x82*"..player.name.."\x82 has died.")
+				if DiscordBot then
+					DiscordBot.Data.msgsrb2 = $ .. "[" .. #player .. "]:skull: **" .. player.name .. "** died.\n"
 				end
 			end
 			if P_RandomChance(FRACUNIT/4) and CV_PTSR.screams.value and lastScreamTic ~= leveltime then

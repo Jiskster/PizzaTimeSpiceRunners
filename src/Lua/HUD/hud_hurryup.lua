79e06c9522efb13d17ce3eed72dfda1994d109d9
Blur <danielthesmartypants2017@gmail.com>
2024-08-07 12:38:50 -0500

    make hurryup hud code more safer

--- src/Lua/HUD/hud_hurryup.lua
+++ src/Lua/HUD/hud_hurryup.lua
@@ -1,12 +1,15 @@
 local hurryup_hud = function(v, player)
+	if not PTSR.timeleft then return end
+	
 	local x = 450*FRACUNIT
 	local y = 100*FRACUNIT
-	
+	local timedif = 20*TICRATE-PTSR.timeleft
 	local timestring = G_TicsToMTIME(PTSR.timeleft)
 	
-	if (PTSR.timeleft <= 20*TICRATE and multiplayer and PTSR.client_allowhurryupmusic) then
-		local timedif = 20*TICRATE-PTSR.timeleft
-		customhud.CustomFontString(v, x-((timedif)*FRACUNIT*3), y, timestring, "PTFNT", nil, "center", FRACUNIT, SKINCOLOR_WHITE)
+	x = $ - ((timedif) * FRACUNIT*3)
+	
+	if (PTSR.timeleft <= 20*TICRATE and multiplayer and PTSR.client_allowhurryupmusic) and x > -200*FRACUNIT then
+		customhud.CustomFontString(v, x, y, timestring, "PTFNT", nil, "center", FRACUNIT, SKINCOLOR_WHITE)
 	end
 end
 

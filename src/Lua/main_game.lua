e0e9f559ce17b00ef0d426a49343dfc466dab5c4
Blur <danielthesmartypants2017@gmail.com>
2025-03-31 22:21:02 -0500

    bring back hard mode

--- src/Lua/main_game.lua
+++ src/Lua/main_game.lua
@@ -185,6 +185,20 @@ PTSR.gm_ptkidmode = PTSR.RegisterGamemode("Retarded PT Kid Mode", {
 	pfspeedmulti = tofixed("1.7"),
 })
 
+PTSR.gm_hardmode = PTSR.RegisterGamemode("Hard Mode", {
+	dustdevil = true,
+	dustdeviltimer = 30*TICRATE,
+	allowrevive = true,
+	overtime_music = "OTMUSE",
+	instant_overtime = true,
+	ring_score = 50,
+	enemy_score = 2000,
+	ringlapbonus = PTSR.ringlapbonus*2,
+	lapbonus = PTSR.lapbonus*4,
+	pfspeedmulti = (FU/4)*5, -- 1.25x
+	overtime_textontime = "HARD MODE!",
+})
+
 PTSR.ChangeGamemode = function(gm)
 	local newgamemode = gm or 1
 	local gm_metadata = PTSR.gamemode_list[gm]

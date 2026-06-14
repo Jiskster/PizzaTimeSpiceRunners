16be82321ac1ab2055c24633eb7bb5f3c95535ec
Jisk <danielthesmartypants2017@gmail.com>
2026-06-13 21:33:00 -0500

    Better and changed logic for ring loss

--- src/Lua/PlayerScripts/player_ringdrophandle.lua
+++ src/Lua/PlayerScripts/player_ringdrophandle.lua
@@ -5,24 +5,24 @@ addHook("MobjDamage", function(target, inflictor, source, damage, damagetype)
 		if not (damagetype & DMG_DEATHMASK) then
 			if not player.ptsr.outofgame
 			and not PTSR.DoHook('ondamage', target, inflictor, source, damage, damagetype) then
+				local damagesound = (player.rings > 0) and sfx_s3kb9 or sfx_shldls
+
 				PTSR:AddComboTime(player, -(2*TICRATE + TICRATE/2))
 				
 				if player.powers[pw_shield] then 
 					return 
 				end
-				
-				if player.rings < 125 then
-					S_StartSound(target, sfx_s3kb9) -- ring loss sound
-					P_PlayerRingBurst(player, 5)
-					player.rings = 0
-					player.powers[pw_shield] = 0
-				else
-					S_StartSound(target, sfx_shldls)
-					P_PlayerRingBurst(player, 32)
-					player.rings = ($*5)/6
+
+				if player.rings > 0 then
+					local ringslost = player.rings - (player.rings/3)
+
+					P_PlayerRingBurst(player, ringslost)
+					player.rings = $ / 3
+
 					player.powers[pw_shield] = 0
 				end
-				
+
+				S_StartSound(target, damagesound)
 				PTSR.DeductScore(player, 500)
 				P_DoPlayerPain(player, source, inflictor)
 			end

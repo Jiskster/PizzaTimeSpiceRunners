f62fb8b0ede1a4dd3d75806a102d01a48dedba2a
Jisk <danielthesmartypants2017@gmail.com>
2026-06-13 21:39:23 -0500

    Add better method of removing score from enemies.

--- src/Lua/Hooks/PlayerTweaks.lua
+++ src/Lua/Hooks/PlayerTweaks.lua
@@ -1,29 +1,14 @@
-local score_table = {
-	[1] = 100,
-	[2] = 200,
-	[3] = 500,
-}
-
--- Doesn't limit to grabbing rings. you get extra score on killing other stuff too
 addHook("MobjDeath", function(target, inflictor, source)
 	if CV_PTSR.scoreonkill.value and PTSR.IsPTSR() and source and source.valid and source.player and source.player.valid then
 		local player = source.player
 		local gm_metadata = PTSR.currentModeMetadata()
 		local ring_score = gm_metadata.ring_score or PTSR.ring_score
-		local real_scoreadd = player.scoreadd + 1
-		local scoreadd_deduct = 0
-
-		if score_table[real_scoreadd] then
-			scoreadd_deduct = score_table[real_scoreadd]
-		elseif real_scoreadd >= 4 and real_scoreadd <= 14 then
-			scoreadd_deduct = 1000
-		elseif real_scoreadd > 14 then
-			scoreadd_deduct = 10000
-		end
+		
+		player.scoreadd = 0
 		
 		if (target.flags & MF_ENEMY) then 
-			player.score = max(0, $-scoreadd_deduct) -- remove score given from enemies
-			
+			target.flags = $ & (~MF_ENEMY) -- so we dont get score from enemies
+
 			if not player.ptsr.pizzaface then
 				PTSR:AddCombo(player)
 				PTSR:AddComboTime(player, player.ptsr.combo_maxtime)
@@ -33,12 +18,14 @@ addHook("MobjDeath", function(target, inflictor, source)
 			if player.ptsr.atrraction_timer then
 				player.ptsr.atrraction_timer = min($ + (TICRATE/2), 15*TICRATE) -- can only go up to 15 seconds.
 			end
+
 			return
 		elseif (target.type == MT_RING or target.type == MT_COIN)
 			player.ptsr.rings_on_lap = $ + 1
+
 			P_AddPlayerScore(player, ring_score)
 			PTSR.add_wts_score(player, target, ring_score)
-			
+
 			if not player.ptsr.pizzaface then
 				PTSR:AddComboTime(player, TICRATE)
 			end
